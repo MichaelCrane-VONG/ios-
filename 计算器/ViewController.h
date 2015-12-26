1165cca5ff0e71c2fe4f271532885cec675bcaff
myApple <myapple@myapple.local>
2015-12-26 13:40:21 +0800

    Initial Commit

---
+++ 计算器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  计算器
+//
+//  Created by myApple on 15/12/26.
+//  Copyright © 2015年 myApple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 计算器/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  计算器
+//
+//  Created by myApple on 15/12/26.
+//  Copyright © 2015年 myApple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
